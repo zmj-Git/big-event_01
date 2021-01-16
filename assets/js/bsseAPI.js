7533bdaabc600f095dd93b25d59f1a6f2fa6a911
zmj <1241311306@qq.com>
2021-01-16 12:20:07 +0800

    主页功能完成

--- assets/js/bsseAPI.js
+++ assets/js/bsseAPI.js
@@ -9,4 +9,21 @@ var baseURL = "http://api-breakingnews-web.itheima.net"
 $.ajaxPrefilter(function (params) {
     //拼接服务器地址
     params.url = baseURL + params.url;
-})
+    if (params.url.indexOf("/my/") !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+
+    //3.拦截所有的响应，判断身份认证信息
+    params.complete = function (res) {
+        console.log(res)
+        console.log(res.responseJSON)
+        var obj = res.responseJSON;
+        if (obj.status == 1 && obj.message == '身份认证失败！') {
+            //1.清空本地token
+            localStorage.removeItem("token");
+            location.href = "/login.html"
+        }
+    }
+});
---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+$(function () {
+    //1.获取用户信息
+    getUserInof();
+    //2.退出
+    var layer = layui.layer;
+    $("#btnLogout").on("click", function () {
+        layer.confirm('是否确认退出?', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem("token");
+            location.href = "/login.html";
+            layer.close(index);
+        });
+    })
+});
+function getUserInof() {
+    $.ajax({
+        url: "/my/userinfo",
+        // //headers属性：用户设置请求头信息
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || "",
+        // },
+        success: function (res) {
+            // console.log(res)
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
+            }
+            //请求成功，渲染头像
+            renderAvatar(res.data);
+        }
+    })
+}
+function renderAvatar(user) {
+    //1.渲染名称（nickname优先，如果没有，就用username）
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    //2.渲染头像
+    if (user.user_pic !== null) {
+        $(".layui-nav-img").show().attr("src", user.user_pic);
+        $(".text-avatar").hide();
+    } else {
+        $(".layui-nav-img").hide();
+        var text = name[0].toUpperCase();
+        $(".text-avatar").show().html(text);
+    }
+}
